1983dff4621b3d688895133eec2ddeb5145f8534
sahanmndl <mondalsahan053@gmail.com>
2022-10-04 16:19:40 +0530

    Kth largest element

---
+++ CPP/Arrays/kth_largest_element.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Problem Link: https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    for(auto x : nums) {
+        minHeap.push(x);
+        if(minHeap.size() > k) {
+            minHeap.pop();
+        }
+    }
+    
+    return minHeap.top();
+}
+
+int main() {
+    vector<int> nums;
+    for(int i = 1; i <= 10; i++) {
+        nums.push_back(i);
+    }
+
+    int k = 3;
+    int ans = findKthLargest(nums, k);
+
+    cout << "kth largest element = " << ans << endl;
+    
+    return 0;
+}
