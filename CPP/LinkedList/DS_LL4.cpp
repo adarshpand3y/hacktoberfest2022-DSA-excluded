a851052386bfb166956068e0d1b59ced5a86bfd1
Om Parulekar <65206237+sanchezsakura11@users.noreply.github.com>
2022-10-05 02:57:52 +0530

    Added file to Linked List Folder

---
+++ CPP/LinkedList/DS_LL4.cpp
@@ -0,0 +1,46 @@
+// Inserting Node At Tail
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* head=NULL;
+
+void InsertNode(int x){
+    Node* temp=new Node();
+    temp->data=x;
+    temp->next=NULL;
+
+    if(head==NULL) head=temp;
+    else{
+        Node* temp1=head;
+        while(temp1->next!=NULL){
+        temp1=temp1->next;
+        }
+        temp1->next=temp;
+    }
+}
+
+void Print(){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        InsertNode(x);
+    }
+    Print();
+}
+
